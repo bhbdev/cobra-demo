040b30c9501bc2a67b70a015d189e0b9e4e281d4
= <=>
2018-06-18 16:44:34 -0400

    revshare wrap up, needs slight styling

--- src/app/revshare/revshare.component.ts
+++ src/app/revshare/revshare.component.ts
@@ -12,6 +12,11 @@ export class RevShareComponent implements OnInit {
 
   public business;
   public months;
+  public revMonths;
+  public revCurrentMonth;
+  public revMonthRevenue;
+  public revTotalRevenue;
+  public DOISubscribers;
   public leadsPerMonth;
   public confirmRate;
   public perUserAvg;
@@ -22,8 +27,6 @@ export class RevShareComponent implements OnInit {
   }
 
   ngOnInit() {
-
-    
     
     this.business = this._data.getBusiness();
     if (!this.business.newsletter) {
@@ -31,17 +34,55 @@ export class RevShareComponent implements OnInit {
       return;
     }
     
-    this.months = Array.from(Array(12),(x,i)=>i+1);
-    this.leadsPerMonth = this.business.optins_per_day * 30;
+    this.calculateValues();
+  }
+  
+  calculateValues() {
     this.confirmRate = 0.12;
     this.perUserAvg = 0.42;
     this.revAvg = 0.85;
-      
+    this.revTotalRevenue = 0;
+    this.months = Array.from(Array(12),(x,i)=>i+1);
+    
+    this.leadsPerMonth = this.business.optins_per_day * 30;
+    this.DOISubscribers = this.leadsPerMonth * this.confirmRate;
+    this.revCurrentMonth = (this.leadsPerMonth * this.confirmRate) * this.perUserAvg;   
+    this.revMonths = [];
+    for (var i=0; i < this.months.length; ++i) 
+    {
+      this.revMonths[i] = [];
+      for (var x=0; x < 12; ++x) {
+        if (i==0) {
+          this.revMonths[i][x] = this.revCurrentMonth;
+        }
+        else {
+          if (x < i)
+            this.revMonths[i][x] = '';
+          else
+            this.revMonths[i][x] = Math.ceil((this.DOISubscribers * (this.revAvg - (0.05 * i))) * this.perUserAvg);
+        }
+      }  
+    }
+    var monthTotals = [];
+    for (var i=0; i < this.revMonths.length; ++i) {
+      var total = 0;
+      for (var x=0; x < this.revMonths.length; ++x) {
+        if (x <= i) {
+          total += this.revMonths[x][i];
+        }
+      }
+      monthTotals.push(total);
+      this.revTotalRevenue += total * .5;
+    }
+    this.revMonthRevenue = monthTotals;
+    
+    console.debug(this.revMonths)
   }
   
-  setEzine() {
+  setLeads() {
     this._data.saveBusiness(this.business);
     this.business = this._data.getBusiness();
+    this.calculateValues();
   }
   
   onBackward() {
